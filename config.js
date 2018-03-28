534a5496322fda6f60a314a4c07e91ccf710e869
Nathan Lipke <nlipke@salesforce.com>
2018-03-28 11:47:10 -0600

    make the event configable

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if
 exports.DEBUG = true;
 exports.ENVIRONMENT = process.env.ENVIRONMENT || 'sandbox';
 exports.CALLBACK_URL = process.env.CALLBACK_URL || 'http://localhost:3001';
-exports.PUSH_TOPIC = 'RequestPartOrder__e';
+exports.PUSH_TOPIC = process.env.PUSH_TOPIC || 'RequestPartOrder__e';
 exports.IS_EVENT = true;
 
 exports.CLIENT_ID = process.env.CLIENT_ID;
